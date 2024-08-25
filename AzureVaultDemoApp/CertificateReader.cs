02f9a317ce19bf363e3e67c8e9c93d789f430e90
Mohamed Ali <mo7amed3ly@gmail.com>
2024-08-25 19:22:55 +0300

    move GetCertificate to a seperate class

---
+++ AzureVaultDemoApp/CertificateReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AzureVaultDemoApp
+{
+    internal class CertificateReader
+    {
+
+        public X509Certificate2 GetCertificate(string thumbprint)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certificateCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+                if (certificateCollection == null || certificateCollection.Count == 0)
+                {
+                    throw new Exception("Certificate is not installed");
+                }
+                return certificateCollection[0];
+            }
+            finally
+            {
+
+                store.Close();
+            }
+        }
+    }
+}
--- AzureVaultDemoApp/Program.cs
+++ AzureVaultDemoApp/Program.cs
@@ -10,28 +10,11 @@ namespace AzureVaultDemoApp
         static void Main(string[] args)
         {
             var appConfig = new ConfigReader().Read();
-            X509Certificate2 certificate = GetCertificate(appConfig.Thumbprint);
+            X509Certificate2 certificate = new CertificateReader().GetCertificate(appConfig.Thumbprint);
             var client = new SecretClient(new System.Uri(appConfig.VaultUri), new ClientCertificateCredential(appConfig.TenantId, appConfig.ClientId, certificate));
             var secret = client.GetSecret(appConfig.SecretName);
-        }
-
-        private static X509Certificate2 GetCertificate(string thumbprint)
-        {
-            var store=new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            try
-            {
-                store.Open(OpenFlags.ReadOnly);
-                var certificateCollection=store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint,false);
-                if (certificateCollection == null || certificateCollection.Count==0) {
-                    throw new Exception("Certificate is not installed");
-                }
-                return certificateCollection[0];
-            }
-            finally
-            {
-
-               store.Close();
-            }
+            Console.WriteLine($"Now I know your birthdat. It is {secret.Value.Value}");
+            Console.ReadKey();
         }
     }
 }
